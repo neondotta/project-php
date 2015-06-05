4bab386d6da291572f92081a438bb63f0fb17134
Neon Dotta <neondotta@gmail.com>
2015-06-05 01:40:02 -0300

    alterando index.php

--- index.php
+++ index.php
@@ -7,11 +7,37 @@
 	<title>Projeto-Code</title>
 
 	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 	<?php require_once("menu.php");?>
 
-
+	<div class="panel panel-default">
+  
+		<div class="panel-heading ctltxt"><h4><strong>Links estudo</strong></h4></div>
+	   	<table class="table ctl98">
+    		<tr>
+    			<td><strong>Nome</strong></td>
+    			<td><strong>Link</strong></td>
+    			<td><strong>OBS</strong></td>
+    		</tr>
+    		<tr>
+    			<td>Code Education</td>
+    			<td><a href="http://portal.code.education/">Portal</a></td>
+    			<td>Videos para estudo</td>
+    		</tr>
+    		<tr>
+    			<td>GitHub</td>
+    			<td><a href="https://github.com/">Página Inicial</a></td>
+    			<td>Repositórios online</td>
+    		</tr>
+    		<tr>
+    			<td>Bootstrap</td>
+    			<td><a href="http://getbootstrap.com/">Página Inicial</a></td>
+    			<td>FrameWork - HTML, CSS e JS</td>
+    		</tr>
+		</table>
+	</div>
 
 	<?php require_once("rodape.php");?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
